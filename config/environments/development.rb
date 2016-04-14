dc899b7adf9612b8fe774e98cf98a60f4a1daf79
Chris Gunn <chrisrgunn@gmail.com>
2016-04-14 01:28:53 +0000

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { :host => "https://bumpinuglies.herokuapp.com"}
+  config.action_mailer.default_url_options = { :host => "bumpinuglies.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
