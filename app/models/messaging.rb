dbde3164d840f549521e972180464b3d99c1c2d7
Chris Gunn <chrisrgunn@gmail.com>
2016-04-14 17:49:49 +0000

    added images to messaging

--- app/models/messaging.rb
+++ app/models/messaging.rb
@@ -1,3 +1,4 @@
 class Messaging < ActiveRecord::Base
-
+    has_attached_file :image, styles: { medium: "300x300>", large: "600x600>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
