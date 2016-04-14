2b3dcf65dbf5d41a9df27d8870ab0b213a5640e3
Chris Gunn <chrisrgunn@gmail.com>
2016-04-14 04:26:39 +0000

    ?

--- app/models/messaging.rb
+++ app/models/messaging.rb
@@ -1,7 +1,4 @@
 class Messaging < ActiveRecord::Base
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
-   validates_presence_of :image
-   validates_attachment :image, :presence => true,
-   :content_type => { :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
