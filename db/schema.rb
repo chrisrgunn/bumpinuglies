320bc76f50249d1fc3dfebf93e2a10d5c4684b9a
Chris Gunn <chrisrgunn@gmail.com>
2016-04-15 22:25:06 +0000

    added events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414174255) do
+ActiveRecord::Schema.define(version: 20160415215419) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.string   "location"
+    t.text     "details"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "messagings", force: :cascade do |t|
     t.string   "image_file_name"
